7c3840398defdc777dc949ec967c99514434febc
Md.Shahriar Hossain Zisan <shahriarhossainbsl@gmail.com>
2025-08-24 15:42:30 +0600

    Add files via upload

---
+++ ap.js
@@ -0,0 +1,21 @@
+const textBox=document.getElementById("textBox");
+const toFahrenheit=document.getElementById("toFahrenheit");
+const toCelcius=document.getElementById("toCelcius");
+const myResult=document.getElementById("myResult");
+let temp;
+
+function convert(){
+    if(toFahrenheit.checked){
+        temp=Number(textBox.value);
+    temp=temp*9/5+32;
+    myResult.textContent=temp.toFixed(1)+"F";
+    }
+    else if(toCelcius.checked){
+        temp=Number(textBox.value);
+    temp=(temp-32)*(5/9);
+    myResult.textContent=temp.toFixed(1)+"C";
+    }
+    else{
+        myResult.textContent=`Select a unit`;
+    }
+}
